4893cf24313287da3f2ef5284249cd1cc7c97394
Alan E.J Easton <alaneje@users.noreply.github.com>
2022-05-09 18:36:17 +0100

    Weapon class
    Weapon classes added for #11

---
+++ Assets/Scripts/Shared Classes/Weapon.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public string WeaponName;
+    public Stats WeaponStats;
+    public int[] SkillList;
+}
